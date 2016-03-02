5c76c0b2cbbd385680938d07f43e2b89c6990c40
Stefan Kerlin <sk@spendit.de>
2016-03-02 13:57:36 +0100

    test 1

--- BitriseTrialTests/BitriseTrialTests.swift
+++ BitriseTrialTests/BitriseTrialTests.swift
@@ -18,7 +18,6 @@ class BitriseTrialTests: XCTestCase {
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        
         super.tearDown()
     }
     
