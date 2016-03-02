a4da5a6e0bda8c9d52bf09bb2ada71ac70886099
Stefan Kerlin <sk@spendit.de>
2016-03-02 12:06:16 +0100

    removed unused comments

--- BitriseTrialTests/BitriseTrialTests.swift
+++ BitriseTrialTests/BitriseTrialTests.swift
@@ -22,8 +22,6 @@ class BitriseTrialTests: XCTestCase {
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
         
         let result = ViewController.getString()
         let expected = "TestString"
